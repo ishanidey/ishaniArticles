b0213348182fd53b01bab9b2c38b913671dfafc8
ishanidey <20ec01038@iitbbs.ac.in>
2023-05-13 22:01:29 +0530

    remove framer motion

--- sections/Poster.jsx
+++ sections/Poster.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 // import { AppWrap } from '../../wrapper';
@@ -8,23 +7,17 @@ import Link from 'next/link';
 
 const Poster = () => (
   <div className="app__header app__flex poster">
-    <motion.div
-      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-      transition={{ duration: 1 }}
-      className="app__header-info"
-    >
-      <h1 className="heading"><Link href="https://ishanidey.netlify.app/">View my portfolio website</Link></h1>
-      <div className="app__header-badge">
-        <div className="badge-cmp app__flex">
-          <div style={{ marginLeft: 20 }}>
-            <p>Hi there, 👋 </p>
-            <p>welcome to </p>
-            <p className="text">ishaniDeyBlogs💡</p>
-            <p> I write articles on web development.</p>
-          </div>
+    <h1 className="heading"><Link href="https://ishanidey.netlify.app/">View my portfolio website</Link></h1>
+    <div className="app__header-badge">
+      <div className="badge-cmp app__flex">
+        <div style={{ marginLeft: 20 }}>
+          <p>Hi there, 👋 </p>
+          <p>welcome to </p>
+          <p className="text">ishaniDeyBlogs💡</p>
+          <p> I write articles on web development.</p>
         </div>
       </div>
-    </motion.div>
+    </div>
   </div>
 );
 
